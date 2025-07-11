a23275f175a516470b04efbc9c6ead404054ea33
Kritika Goyal <kritikagoyal@adobe.com>
2025-07-11 16:21:50 +0530

    chat launcher initial commit

---
+++ blocks/chat-launcher/chat-launcher.js
@@ -0,0 +1,18 @@
+export default function decorate(block) {
+  // Create the launcher container
+  const launcher = document.createElement('div');
+  launcher.className = 'chat-launcher-floating';
+
+  // Create the image
+  const img = document.createElement('img');
+  img.src = '/icons/chat-launcher.png';
+  img.alt = 'Chat Launcher';
+  img.className = 'chat-launcher-icon';
+
+  // Append image to container
+  launcher.appendChild(img);
+
+  // Clear block and append launcher
+  block.innerHTML = '';
+  block.appendChild(launcher);
+}
